0c371e48e1766edcb2589dc5ee57c7b6f5e75845
Paulo França <paulovinicius.franca@gmail.com>
2022-03-26 12:59:24 -0300

    Acerta o Eslint

--- app/backend/src/database/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var sequelize_1 = require("sequelize");
-var databaseConfig = require('../config/database');
-exports["default"] = new sequelize_1.Sequelize(databaseConfig);
